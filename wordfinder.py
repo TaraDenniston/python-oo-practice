b1fa22126cae3300a028e6666058fd1768229b8c
Tara Denniston <taradenniston@gmail.com>
2022-10-29 13:22:21 -0400

    completed wordfinder exercise

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,30 @@
-"""Word Finder: finds random words from a dictionary."""
+from random import choice
 
 
 class WordFinder:
-    ...
+    """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.words = []
+        self.readfile()
+
+    def readfile(self):
+        """Read the file, append each word read to the words list, and
+        return the number of words read"""
+        num_words = 0
+
+        f = open(self.filepath, 'r')
+
+        for line in f:
+            self.words.append(line.strip())
+            num_words += 1
+
+        f.close()
+
+        print(f'{num_words} words read')
+
+    def random(self):
+        """Return a random word from the list of words"""
+        return choice(self.words)
+
