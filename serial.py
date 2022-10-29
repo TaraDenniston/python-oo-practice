bf2be87561de8358b8754123643902b06a6b4fae
Tara Denniston <taradenniston@gmail.com>
2022-10-29 12:51:24 -0400

    completed serial exercise

--- serial.py
+++ serial.py
@@ -19,3 +19,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Create a new SerialGenerator object and initialize starting value"""
+        self.start = start
+        self.num = start
+        self.incr = 0
+
+    def generate(self):
+        """Return the next sequential number"""
+        self.num = self.start + self.incr
+        self.incr += 1
+        return self.num
+
+    def reset(self):
+        """Reset the number back to the original start number"""
+        self.num = self.start
+        self.incr = 0
