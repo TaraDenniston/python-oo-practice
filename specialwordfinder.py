ae2319b06ba3451213e4e81d14e48be68c3fdb7e
Tara Denniston <taradenniston@gmail.com>
2022-10-29 15:39:28 -0400

    added files for SpecialWordFinder exercise

---
+++ specialwordfinder.py
@@ -0,0 +1,12 @@
+from wordfinder import WordFinder
+
+class SpecialWordFinder(WordFinder):
+    """
+    A Random Word Finder that works with files that include blank lines and comments
+    """
+
+    def process_line(self, line):
+        """Read one line and return just the word/text without extra characters.
+        Ignore blank lines or lines beginning with '#'"""
+        if line[0] != '#' and line[0] != '':
+            return line.strip()
