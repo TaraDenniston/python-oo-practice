dc92e8ebc4bfcfc10e5ed40642a0b78a42fe078f
Tara Denniston <taradenniston@gmail.com>
2022-10-29 15:42:27 -0400

    made minor tweak to readfile method

--- wordfinder.py
+++ wordfinder.py
@@ -7,9 +7,9 @@ class WordFinder:
     def __init__(self, filepath):
         self.filepath = filepath
         self.words = []
-        self.readfile()
+        self.readfile(self.process_line)
 
-    def readfile(self):
+    def readfile(self, process_function):
         """Read the file, append each word read to the words list, and
         return the number of words read"""
         num_words = 0
@@ -17,12 +17,18 @@ class WordFinder:
         f = open(self.filepath, 'r')
 
         for line in f:
-            self.words.append(line.strip())
-            num_words += 1
+            word = process_function(line)
+            if word:
+                self.words.append(word)
+                num_words += 1
 
         f.close()
 
         print(f'{num_words} words read')
+
+    def process_line(self, line):
+        """Read one line and return just the word/text without extra characters"""
+        return line.strip()
 
     def random(self):
         """Return a random word from the list of words"""
